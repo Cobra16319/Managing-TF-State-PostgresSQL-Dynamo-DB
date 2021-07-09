ed2ba320433d956862b31babd95ef593221e8034
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-09 12:41:07 -0400

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -1,7 +1,7 @@
 # Trying to do one four loop for whole script as a test
 #!/bin/bash
 
-for P in {1..3} ; do echo export DOCKER_BUILDKIT=1 && docker buildx create --use --name=qemu && docker buildx inspect --bootstrap && vagrant up --provider=docker && vagrant ssh ; done
+for P in {1..5} ; do echo export DOCKER_BUILDKIT=1 && docker buildx create --use --name=qemu && docker buildx inspect --bootstrap && vagrant up --provider=docker && vagrant ssh ; done
 
 
 
