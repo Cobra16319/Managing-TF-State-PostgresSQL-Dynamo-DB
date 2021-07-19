2d970f79a2fd60e6dfc7e74e861f84225bbb59ea
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-19 09:16:25 -0400

    Update shell script
    Fixed the issue with terraform install

--- install.sh
+++ install.sh
@@ -1,13 +1,32 @@
 #bin/bash
 
-for i in 1 2 3 4 5 6 7 8 9; do 
-  sudo apt-get -y install yum-utils
-  sudo apt-get -y --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do 
+  #Adding HashiCorp Repo & Verify GPG signature
+  
+  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
+  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+ 
+ # Logging issue on this vagrant install terraform not loading. Adding work around
+  
   sudo apt-get -y install terraform
   sudo apt-get -y install nomad
   sudo apt-get -y install vault
   sudo apt-get -y install packer
   sudo apt-get -y install consul
+  
+  # Should already be installed but confirm latest (will fix) and install docker compose
+  
   sudo apt-get -y install python3-pip
   sudo pip install docker-compose
+  
+  # Verify version of software installed
+  
+  terraform version
+  nomad version
+  vault version
+  packer version
+  consul version
+  
+  
 done
