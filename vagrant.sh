e6d1534f75703578751795c0584a1508eb59a414
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-19 05:37:53 -0400

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -1,4 +1,5 @@
 # Trying to do one four loop for whole script as a test. 
+# Depending on your OS you may not need the first 3 commands if script fails # them out and use virtual box.
 # Feed back welcome already works and installed need to add git hub actions later. 
 #!/bin/bash
 
