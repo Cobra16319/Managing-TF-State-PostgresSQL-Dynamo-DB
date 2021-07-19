3cb9fbbb089a97bb079280a5b2289c3a1c473841
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-19 06:09:15 -0400

    Updating Script

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #bin/bash
 
 for i in 1 2 3 4 5 6 7 8 9; do 
-  sudo yum install -y yum-utils
-  sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-  sudo yum -y install terraform
-  sudo yum -y install nomad
-  sudo yum -y install vault
-  sudo yum -y install packer
-  sudo yum -y install consul
+  sudo apt-get -y install yum-utils
+  sudo apt-get -y --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+  sudo apt-get -y install terraform
+  sudo apt-get -y install nomad
+  sudo apt-get -y install vault
+  sudo apt-get -y install packer
+  sudo apt-get -y install consul
   sudo apt-get -y install python3-pip
   sudo pip install docker-compose
 done
