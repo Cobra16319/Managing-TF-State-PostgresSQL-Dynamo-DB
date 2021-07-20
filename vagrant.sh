2f0aa7226e15fbc796cf792ba7fb3f82efd5dd5a
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-20 08:08:06 -0400

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -3,10 +3,7 @@
 # Feed back welcome already works and installed need to add git hub actions later. 
 #!/bin/bash
 
-for i in 1 2 3 4 5; do 
-  echo export DOCKER_BUILDKIT=1 && 
-  docker buildx create --use --name=qemu && 
-  docker buildx inspect --bootstrap && 
+for i in 1 2; do 
   vagrant up --provider=docker && 
   vagrant ssh  
 done
