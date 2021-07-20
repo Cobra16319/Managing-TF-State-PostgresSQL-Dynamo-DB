0ba5e6449a81c007cd8bd5222084a00672a2eb85
Greg <46206055+Cobra16319@users.noreply.github.com>
2021-07-20 11:44:54 -0400

    Update and rename install.sh to post-install.sh

--- post-install.sh
+++ post-install.sh
@@ -1,9 +1,10 @@
 # HashiStack Shell Script
 #bin/bash
 
-for i in 1 2 3 4 5 6 7 8 9 10 11 12 13; do 
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14; do 
   #Adding HashiCorp Repo & Verify GPG signature
   
+  sudo apt install docker-compose
   sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
   curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
   sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
